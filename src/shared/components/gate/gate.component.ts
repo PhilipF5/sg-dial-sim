5c8d34511c4eb0f2a79e5e3168f1043806de28c8
Philip Fulgham <philfulgham@gmail.com>
2018-05-28 10:38:51 -0400

    Adjust kawoosh animation for better timing

--- src/shared/components/gate/gate.component.ts
+++ src/shared/components/gate/gate.component.ts
@@ -1,6 +1,6 @@
 import { Component, ElementRef, Input, ViewChild, SimpleChanges } from "@angular/core";
 
-import { Power1, Power4, TimelineLite, TweenMax } from "gsap";
+import { Elastic, Power1, Power4, TimelineLite, TweenMax } from "gsap";
 
 @Component({
 	selector: "gate",
@@ -35,8 +35,8 @@ export class GateComponent {
 			case "ACTIVE":
 				timeline.to(this.eventHorizon.nativeElement, 0.5, { scale: 0, ease: Power4.easeOut });
 				timeline.set(this.eventHorizon.nativeElement, { css: { className: "+=active" } });
-				timeline.to(this.eventHorizon.nativeElement, 1, { scale: 4, ease: Power1.easeIn });
-				timeline.to(this.eventHorizon.nativeElement, 1, { scale: 3, ease: Power1.easeInOut });
+				timeline.to(this.eventHorizon.nativeElement, 1, { scale: 4, ease: Power1.easeInOut });
+				timeline.to(this.eventHorizon.nativeElement, 2.5, { scale: 3, ease: Elastic.easeInOut });
 				timeline.add(TweenMax.to(this.eventHorizon.nativeElement, 1, { scale: 2.75, ease: Power1.easeInOut })
 					.repeat(-1)
 					.yoyo(true)
