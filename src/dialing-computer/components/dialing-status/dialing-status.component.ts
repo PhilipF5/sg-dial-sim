521714f4a3fb34568f2853b19dbbb130a953c0af
Philip Fulgham <philfulgham@gmail.com>
2018-05-28 10:04:05 -0400

    Add change logic and animation to dialing status

--- src/dialing-computer/components/dialing-status/dialing-status.component.ts
+++ src/dialing-computer/components/dialing-status/dialing-status.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input } from "@angular/core";
+import { Component, ElementRef, Input, SimpleChanges, ViewChild } from "@angular/core";
+
+import { TimelineLite, TweenMax } from "gsap";
 
 @Component({
 	selector: "dialing-status",
@@ -8,4 +10,53 @@ import { Component, Input } from "@angular/core";
 export class DialingStatusComponent {
 	@Input()
 	public status: string;
+
+	@ViewChild("statusText")
+	private statusText: ElementRef;
+
+	ngOnChanges(changes: SimpleChanges) {
+		if (changes.status && changes.status.previousValue !== changes.status.currentValue) {
+			this.killAnimation();
+			switch (this.status) {
+				case "IDLE":
+				case "ACTIVE":
+					this.flashNormal();
+					break;
+				case "ENGAGED":
+					this.flashOnce();
+					break;
+				case "DIALING":
+					this.hide();
+					break;
+			}
+		}
+	}
+
+	private flashNormal(): TweenMax {
+		return TweenMax.fromTo(this.statusText.nativeElement, 1, { opacity: 0 }, { opacity: 1 })
+			.repeat(-1)
+			.yoyo(true);
+	}
+
+	private flashOnce(): TweenMax {
+		return TweenMax.fromTo(this.statusText.nativeElement, 1, { opacity: 0 }, { opacity: 1 })
+			.repeat(1)
+			.repeatDelay(3)
+			.yoyo(true);
+	}
+
+	private flashWarning(): TweenMax {
+		TweenMax.set(this.statusText.nativeElement, { css: { className: "+=warning" } });
+		return TweenMax.fromTo(this.statusText.nativeElement, 1, { opacity: 0 }, { opacity: 1 })
+			.repeat(-1)
+			.yoyo(true);
+	}
+
+	private hide(): void {
+		TweenMax.set(this.statusText.nativeElement, { opacity: 0 });
+	}
+
+	private killAnimation(): void {
+		TweenMax.killTweensOf(this.statusText.nativeElement);
+	}
 }
