7ccee13c4f153da66a1381739219763c6257807e
Philip Fulgham <philfulgham@gmail.com>
2018-06-09 14:09:05 -0400

    Allow fixed-width segment displays

--- src/shared/components/segment-display/segment-display.component.ts
+++ src/shared/components/segment-display/segment-display.component.ts
@@ -6,10 +6,21 @@ import { Component, Input } from "@angular/core";
 	styleUrls: ["./segment-display.component.scss"]
 })
 export class SegmentDisplayComponent {
+	@Input() align: string;
 	@Input() border: string = "both";
+	@Input() size: number;
 	@Input() text: string = "";
 
 	public get chars(): string[] {
-		return this.text.split("");
+		let text = this.text;
+		if (this.size) {
+			if (this.align === "right") {
+				text = text.padStart(this.size);
+			}
+			else {
+				text = text.padEnd(this.size);
+			}
+		}
+		return text.split("");
 	}
 }
