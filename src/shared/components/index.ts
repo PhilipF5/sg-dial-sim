e9fabd873e89e6946f57c12610be62771274ebf9
Philip Fulgham <philfulgham@gmail.com>
2018-07-29 13:56:49 -0400

    Add scrolling bits display to left column

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -3,6 +3,7 @@ import { AlertFactoryComponent } from "./alert-factory/alert-factory.component";
 import { EventHorizonComponent } from "./event-horizon/event-horizon.component";
 import { GateComponent } from "./gate/gate.component";
 import { MenuComponent } from "./menu/menu.component";
+import { ScrollingBitsComponent } from "./scrolling-bits/scrolling-bits.component";
 import { SegmentDisplayComponent } from "./segment-display/segment-display.component";
 
 export { AlertComponent } from "./alert/alert.component";
@@ -10,6 +11,7 @@ export { AlertFactoryComponent } from "./alert-factory/alert-factory.component";
 export { EventHorizonComponent } from "./event-horizon/event-horizon.component";
 export { GateComponent } from "./gate/gate.component";
 export { MenuComponent } from "./menu/menu.component";
+export { ScrollingBitsComponent } from "./scrolling-bits/scrolling-bits.component";
 export { SegmentDisplayComponent } from "./segment-display/segment-display.component";
 
 export const COMPONENTS = [
@@ -18,5 +20,6 @@ export const COMPONENTS = [
 	EventHorizonComponent,
 	GateComponent,
 	MenuComponent,
+	ScrollingBitsComponent,
 	SegmentDisplayComponent,
 ];
---
+++ src/shared/components/scrolling-bits/scrolling-bits.component.ts
@@ -0,0 +1,80 @@
+import { ChangeDetectorRef, Component, ElementRef, Input, ViewChild } from "@angular/core";
+
+import { Linear, TimelineLite, TimelineMax, TweenLite } from "gsap";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+
+const plugins = [ScrollToPlugin];
+
+@Component({
+	selector: "scrolling-bits",
+	templateUrl: "./scrolling-bits.component.html",
+	styleUrls: ["./scrolling-bits.component.scss"],
+})
+export class ScrollingBitsComponent {
+	public bits: string = "";
+	@Input() public length: number;
+
+	public get enabled(): boolean {
+		return this._enabled;
+	}
+
+	@Input()
+	public set enabled(value: boolean) {
+		if (value === this._enabled) {
+			return;
+		} else if (value) {
+			this.enable();
+		}
+
+		this._enabled = value;
+	}
+
+	private _enabled: boolean;
+	@ViewChild("first") private _firstGroup: ElementRef;
+	@ViewChild("second") private _secondGroup: ElementRef;
+	private timeline: TimelineLite;
+
+	private get elem(): HTMLElement {
+		return this._elem.nativeElement;
+	}
+
+	private get firstGroup(): HTMLElement {
+		return this._firstGroup.nativeElement;
+	}
+
+	private get secondGroup(): HTMLElement {
+		return this._secondGroup.nativeElement;
+	}
+
+	constructor(private cdRef: ChangeDetectorRef, private _elem: ElementRef) {}
+
+	public disable(): void {
+		this.bits = "";
+		this.timeline.kill();
+		TweenLite.set(this.firstGroup, { opacity: 0 });
+	}
+
+	public enable(): void {
+		this.loadBits();
+		setTimeout(() => this.animate(), 100);
+	}
+
+	private animate(): void {
+		this.cdRef.detectChanges();
+		this.timeline = new TimelineMax({ repeat: -1 })
+			.to(this.elem, 10, { scrollTo: this.secondGroup.offsetTop, ease: Linear.easeNone })
+			.add(() => TweenLite.set(this.firstGroup, { opacity: 1 }))
+			.set(this.elem, { scrollTo: 0 });
+	}
+
+	private loadBits(): void {
+		for (let i = 0; i < this.length; i++) {
+			if (Math.random() > 0.5) {
+				this.bits += "1";
+			} else {
+				this.bits += "0";
+			}
+			this.cdRef.detectChanges();
+		}
+	}
+}
