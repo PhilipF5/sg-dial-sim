4daec0a7cf34fcc51d71abffc488db58ba3edeaf
Philip Fulgham <philfulgham@gmail.com>
2018-07-30 19:55:53 -0400

    Create reusable tooltip component

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -5,6 +5,7 @@ import { GateComponent } from "./gate/gate.component";
 import { MenuComponent } from "./menu/menu.component";
 import { ScrollingBitsComponent } from "./scrolling-bits/scrolling-bits.component";
 import { SegmentDisplayComponent } from "./segment-display/segment-display.component";
+import { TooltipComponent } from "./tooltip/tooltip.component";
 
 export { AlertComponent } from "./alert/alert.component";
 export { AlertFactoryComponent } from "./alert-factory/alert-factory.component";
@@ -13,6 +14,7 @@ export { GateComponent } from "./gate/gate.component";
 export { MenuComponent } from "./menu/menu.component";
 export { ScrollingBitsComponent } from "./scrolling-bits/scrolling-bits.component";
 export { SegmentDisplayComponent } from "./segment-display/segment-display.component";
+export { TooltipComponent } from "./tooltip/tooltip.component";
 
 export const COMPONENTS = [
 	AlertComponent,
@@ -22,4 +24,5 @@ export const COMPONENTS = [
 	MenuComponent,
 	ScrollingBitsComponent,
 	SegmentDisplayComponent,
+	TooltipComponent,
 ];
---
+++ src/shared/components/tooltip/tooltip.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+	selector: "tooltip",
+	templateUrl: "./tooltip.component.html",
+	styleUrls: ["./tooltip.component.scss"]
+})
+export class TooltipComponent {}
