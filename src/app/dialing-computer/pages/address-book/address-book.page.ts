d0d82d37a9ccac86796e47ab922606beeba17c51
Philip Fulgham <philfulgham@gmail.com>
2018-12-20 20:39:54 -0500

    Create moving selector for address book

--- src/app/dialing-computer/pages/address-book/address-book.page.ts
+++ src/app/dialing-computer/pages/address-book/address-book.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+
+import { TimelineLite, TweenLite } from "gsap";
 
 import { Destination } from "app/shared/models";
 import { GateNetworkService } from "app/shared/services";
@@ -9,12 +11,30 @@ import { GateNetworkService } from "app/shared/services";
 	styleUrls: ["./address-book.page.scss"]
 })
 export class AddressBookPage implements OnInit {
+	@ViewChild("selector") _selector: ElementRef;
+
 	public destinations: Destination[];
 	public glyphHeadings = Array.from("123456");
+
+	private get selector(): HTMLElement {
+		return this._selector.nativeElement;
+	}
 
 	constructor(private gateNetwork: GateNetworkService) {}
 
 	ngOnInit() {
 		this.destinations = this.gateNetwork.getAllAddresses();
+	}
+
+	public moveSelector(target: HTMLElement): TimelineLite {
+		let topOfTarget = target.getBoundingClientRect().top;
+		return new TimelineLite()
+			.to(this.selector, 0.5, { top: topOfTarget })
+			.set(target, { className: "+=selected" });
+	}
+
+	public removeSelector(target: HTMLElement): TimelineLite {
+		TweenLite.killTweensOf(target);
+		return new TimelineLite().set(target, { className: "-=selected" })
 	}
 }
