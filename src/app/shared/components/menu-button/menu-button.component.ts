e147c7fcd267e3a351da65ca37c1e0fa8c92d717
Philip Fulgham <philfulgham@gmail.com>
2018-12-22 11:38:33 -0500

    Close menu-button menu on sg-button click

--- src/app/shared/components/menu-button/menu-button.component.ts
+++ src/app/shared/components/menu-button/menu-button.component.ts
@@ -1,20 +1,35 @@
-import { Component, Input, ViewChild } from "@angular/core";
+import { AfterContentChecked, Component, ContentChildren, Input, QueryList, ViewChild } from "@angular/core";
+
 import { TweenMax } from "gsap";
+import { Subject } from "rxjs";
+import { takeUntil } from "rxjs/operators";
 
 import { MenuComponent } from "../menu/menu.component";
+import { SgButtonComponent } from "../sg-button/sg-button.component";
 
 @Component({
 	selector: "menu-button",
 	templateUrl: "./menu-button.component.html",
 	styleUrls: ["./menu-button.component.scss"]
 })
-export class MenuButtonComponent {
+export class MenuButtonComponent implements AfterContentChecked {
+	@ContentChildren(SgButtonComponent, { descendants: true }) buttons: QueryList<SgButtonComponent>;
+
 	@Input() position: string;
 	@Input() text: string;
 
 	@ViewChild(MenuComponent) menu: MenuComponent;
 
 	public isMenuOpen: boolean;
+
+	private killSubscriptions = new Subject();
+
+	ngAfterContentChecked() {
+		this.killSubscriptions.next();
+		this.buttons.forEach(btn => {
+			btn.sgClick.pipe(takeUntil(this.killSubscriptions)).subscribe(() => this.toggleMenu());
+		});
+	}
 
 	public toggleMenu(): void {
 		this.isMenuOpen = !this.isMenuOpen;
