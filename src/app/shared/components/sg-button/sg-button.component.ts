7148a79fa0dfc0e9e136d63c45a1653cb90d4cd0
Philip Fulgham <philfulgham@gmail.com>
2018-12-22 10:45:22 -0500

    Use custom click events for sg-button

--- src/app/shared/components/sg-button/sg-button.component.ts
+++ src/app/shared/components/sg-button/sg-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { Component, EventEmitter, HostListener, Input, Output } from "@angular/core";
 
 @Component({
 	selector: "sg-button",
@@ -7,4 +7,11 @@ import { Component, Input } from "@angular/core";
 })
 export class SgButtonComponent {
 	@Input() text: string;
+
+	@Output() sgClick = new EventEmitter();
+
+	@HostListener("click")
+	public onClick() {
+		this.sgClick.emit();
+	}
 }
