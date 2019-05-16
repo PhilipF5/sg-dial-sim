3173d18c2c76a340cf58e8be5a7a2b890569fd00
Philip Fulgham <philip@philipf5.dev>
2019-05-16 10:47:16 -0400

    Create initial selectors for dialing computer

---
+++ src/app/dialing-computer/selectors/dialing-computer.selectors.ts
@@ -0,0 +1,20 @@
+import { createFeatureSelector, createSelector } from "@ngrx/store";
+import { DialingComputerState } from "app/dialing-computer/state";
+
+export const selectDialingComputerState = createFeatureSelector<DialingComputerState>("dialingComputer");
+
+export const getAddressGlyphAtIndex = (index: number) =>
+	createSelector(
+		selectDialingComputerState,
+		state => state.address[index]
+	);
+
+export const getNextChevron = createSelector(
+	selectDialingComputerState,
+	state => state.nextSymbol + 1
+);
+
+export const getNextGlyph = createSelector(
+	selectDialingComputerState,
+	state => state.address[state.nextSymbol]
+);
