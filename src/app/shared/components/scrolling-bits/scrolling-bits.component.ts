c74f6ce073e3ba59e2825927218c2bfffd2b6e3d
Philip Fulgham <philip@philipf5.dev>
2019-12-17 22:51:29 -0500

    Update scrolling bits animation for GSAP 3

--- src/app/shared/components/scrolling-bits/scrolling-bits.component.ts
+++ src/app/shared/components/scrolling-bits/scrolling-bits.component.ts
@@ -1,9 +1,7 @@
 import { ChangeDetectorRef, Component, ElementRef, Input, OnInit, ViewChild } from "@angular/core";
-import { Linear, TimelineLite, TimelineMax, TweenLite } from "gsap";
+import { gsap } from "gsap";
 import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 import { ElectronService } from "ngx-electron";
-
-const plugins = [ScrollToPlugin];
 
 @Component({
 	selector: "sg-scrolling-bits",
@@ -19,7 +17,7 @@ export class ScrollingBitsComponent implements OnInit {
 	public bits: string = "";
 
 	private _enabled: boolean;
-	private timeline: TimelineLite;
+	private timeline: gsap.core.Timeline;
 
 	public get enabled(): boolean {
 		return this._enabled;
@@ -51,7 +49,9 @@ export class ScrollingBitsComponent implements OnInit {
 	constructor(private cdRef: ChangeDetectorRef, private electron: ElectronService, private _elem: ElementRef) {}
 
 	ngOnInit() {
+		gsap.registerPlugin(ScrollToPlugin);
 		if (this.electron.isElectronApp) {
+			console.log(this.electron);
 			let electronWindow = this.electron.remote.BrowserWindow.getFocusedWindow();
 			electronWindow
 				.addListener("resize", () => this.restart())
@@ -64,7 +64,7 @@ export class ScrollingBitsComponent implements OnInit {
 		this.bits = "";
 		this.timeline.progress(0);
 		this.timeline.kill();
-		TweenLite.set(this.firstGroup, { opacity: 0 });
+		gsap.set(this.firstGroup, { opacity: 0 });
 	}
 
 	public enable(): void {
@@ -81,9 +81,10 @@ export class ScrollingBitsComponent implements OnInit {
 
 	private animate(): void {
 		this.cdRef.detectChanges();
-		this.timeline = new TimelineMax({ repeat: -1 })
-			.to(this.elem, 10, { scrollTo: this.secondGroup.offsetTop, ease: Linear.easeNone })
-			.add(() => TweenLite.set(this.firstGroup, { opacity: 1 }))
+		this.timeline = gsap
+			.timeline({ repeat: -1 })
+			.to(this.elem, 10, { scrollTo: this.secondGroup.offsetTop, ease: "none" })
+			.add(() => gsap.set(this.firstGroup, { opacity: 1 }))
 			.set(this.elem, { scrollTo: 0 });
 	}
 
