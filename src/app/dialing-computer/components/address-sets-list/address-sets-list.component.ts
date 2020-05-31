8132d668f27cc007752405f8107ad140e059b489
Philip Fulgham <philip@philipf5.dev>
2020-05-30 20:55:46 -0400

    Create editable segment display for new address sets

--- src/app/dialing-computer/components/address-sets-list/address-sets-list.component.ts
+++ src/app/dialing-computer/components/address-sets-list/address-sets-list.component.ts
@@ -9,9 +9,19 @@ import { GateNetworkService } from "app/shared/services";
 })
 export class AddressSetsListComponent implements OnInit {
 	public addressSets: Readonly<AddressSet>[];
+	public newSet: string = "";
+
 	public constructor(private gateNetwork: GateNetworkService) {}
 
 	ngOnInit() {
 		this.addressSets = this.gateNetwork.getAddressSets();
+	}
+
+	public onNewKeydown(event: KeyboardEvent): void {
+		if (event.key.match(/^[A-Z ]$/i) && this.newSet.length < 10) {
+			this.newSet += event.key;
+		} else if (event.key === "Backspace" && this.newSet.length > 0) {
+			this.newSet = this.newSet.substring(0, this.newSet.length - 1);
+		}
 	}
 }
