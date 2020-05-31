f0ba2a2a09e252b7aa686cd6d1004f0fe315d7e6
Philip Fulgham <philip@philipf5.dev>
2020-05-30 22:02:10 -0400

    Wire up controls to add/delete address sets

--- src/app/dialing-computer/components/address-sets-list/address-sets-list.component.ts
+++ src/app/dialing-computer/components/address-sets-list/address-sets-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { SegmentDisplayComponent } from "app/shared/components";
 import { AddressSet } from "app/shared/models/address-set.model";
-import { GateNetworkService } from "app/shared/services";
+import { AlertService, GateNetworkService } from "app/shared/services";
 
 @Component({
 	selector: "sg-address-sets-list",
@@ -11,10 +12,20 @@ export class AddressSetsListComponent implements OnInit {
 	public addressSets: Readonly<AddressSet>[];
 	public newSet: string = "";
 
-	public constructor(private gateNetwork: GateNetworkService) {}
+	@ViewChild(SegmentDisplayComponent, { static: true }) newField: SegmentDisplayComponent;
+
+	public constructor(private alert: AlertService, private gateNetwork: GateNetworkService) {}
 
 	ngOnInit() {
-		this.addressSets = this.gateNetwork.getAddressSets();
+		this.updateAddressSets();
+	}
+
+	public onDeleteClick(name: string): void {
+		this.deleteAddressSet(name);
+	}
+
+	public onNewClick(): void {
+		this.createAddressSet();
 	}
 
 	public onNewKeydown(event: KeyboardEvent): void {
@@ -22,6 +33,35 @@ export class AddressSetsListComponent implements OnInit {
 			this.newSet += event.key;
 		} else if (event.key === "Backspace" && this.newSet.length > 0) {
 			this.newSet = this.newSet.substring(0, this.newSet.length - 1);
+		} else if (event.key === "Enter") {
+			this.createAddressSet();
 		}
+	}
+
+	private createAddressSet(): void {
+		if (this.newSet.trim()) {
+			this.gateNetwork.createAddressSet(this.newSet);
+			this.newSet = "";
+			this.newField.elem.blur();
+			this.updateAddressSets();
+		}
+	}
+
+	private deleteAddressSet(name: string): void {
+		if (name === "Default") {
+			this.alert.alerts.next({
+				critical: true,
+				duration: 4000,
+				message: "Invalid Operation",
+				title: "Cannot delete default source",
+			});
+		} else {
+			this.gateNetwork.deleteAddressSet(name);
+			this.updateAddressSets();
+		}
+	}
+
+	private updateAddressSets(): void {
+		this.addressSets = this.gateNetwork.getAddressSets();
 	}
 }
