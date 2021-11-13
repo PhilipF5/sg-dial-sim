924d7b7e2346e535a26fc453a4bb1c3e5b5e68c5
Philip Fulgham <philip@philipf5.dev>
2021-11-13 16:02:56 -0500

    Update to Angular/NgRx 11

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from "@angular/router";
 const routes: Routes = [{ path: "", redirectTo: "dialing-computer/gate-screen", pathMatch: "full" }];
 
 @NgModule({
-	imports: [RouterModule.forRoot(routes, { useHash: true })],
+	imports: [RouterModule.forRoot(routes, { useHash: true, relativeLinkResolution: "legacy" })],
 	exports: [RouterModule],
 })
 export class AppRoutingModule {}
