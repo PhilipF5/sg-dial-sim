c733319645569f25ce08770c933ffc3f9788b034
Philip Fulgham <philip@philipf5.dev>
2021-11-14 20:54:06 -0500

    Simplify scrolling bits

--- src/app/shared/components/scrolling-bits/scrolling-bits.component.ts
+++ src/app/shared/components/scrolling-bits/scrolling-bits.component.ts
@@ -1,25 +1,16 @@
-import { ChangeDetectorRef, Component, ElementRef, Input, OnDestroy, OnInit, ViewChild } from "@angular/core";
-import { ElectronService } from "app/shared/services";
-import { gsap } from "gsap";
-import { ScrollToPlugin } from "gsap/ScrollToPlugin";
-import { Subject } from "rxjs";
-import { takeUntil } from "rxjs/operators";
+import { Component, Input, OnInit } from "@angular/core";
 @Component({
 	selector: "sg-scrolling-bits",
 	templateUrl: "./scrolling-bits.component.html",
 	styleUrls: ["./scrolling-bits.component.scss"],
 })
-export class ScrollingBitsComponent implements OnDestroy, OnInit {
+export class ScrollingBitsComponent implements OnInit {
 	@Input() length: number;
-
-	@ViewChild("first", { static: true }) private _firstGroup: ElementRef;
-	@ViewChild("second", { static: true }) private _secondGroup: ElementRef;
 
 	public bits: string = "";
 
 	private _enabled: boolean = null;
-	private killSubscriptions: Subject<{}> = new Subject();
-	private timeline: gsap.core.Timeline;
+	private _interval: any;
 
 	public get enabled(): boolean {
 		return this._enabled;
@@ -31,52 +22,24 @@ export class ScrollingBitsComponent implements OnDestroy, OnInit {
 			return;
 		} else if (value && this._enabled !== null) {
 			this.enable();
+		} else if (!value && this._enabled) {
+			this.disable();
 		}
 
 		this._enabled = value;
 	}
 
-	private get elem(): HTMLElement {
-		return this._elem.nativeElement;
-	}
-
-	private get firstGroup(): HTMLElement {
-		return this._firstGroup.nativeElement;
-	}
-
-	private get secondGroup(): HTMLElement {
-		return this._secondGroup.nativeElement;
-	}
-
-	constructor(private cdRef: ChangeDetectorRef, private electron: ElectronService, private _elem: ElementRef) {}
-
-	ngOnDestroy() {
-		this.killSubscriptions.next();
-	}
-
 	ngOnInit() {
-		gsap.registerPlugin(ScrollToPlugin);
-		this.electron.windowSizeChanges$.pipe(takeUntil(this.killSubscriptions)).subscribe(() => this.restart());
 		this.enabled && this.enable();
 	}
 
 	public disable(): void {
-		this.bits = "";
-		this.timeline.progress(0);
-		this.timeline.kill();
-		gsap.set(this.firstGroup, { opacity: 0 });
+		clearInterval(this._interval);
 	}
 
 	public enable(): void {
 		this.loadBits();
-		setInterval(() => this.loadBits(), 500);
-	}
-
-	public restart(): void {
-		this.disable();
-		if (this.enabled) {
-			this.enable();
-		}
+		this._interval = setInterval(() => this.loadBits(), 500);
 	}
 
 	private loadBits(): void {
@@ -85,6 +48,5 @@ export class ScrollingBitsComponent implements OnDestroy, OnInit {
 			bits += Math.random() > 0.5 ? "1" : "0";
 		}
 		this.bits = bits;
-		this.cdRef.detectChanges();
 	}
 }
