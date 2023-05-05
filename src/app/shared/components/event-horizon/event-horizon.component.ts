76a4b7fce7cf4988736c12525ae3c96e311cdd0c
Philip Fulgham <philip@philipf5.dev>
2023-05-04 23:17:02 -0400

    Clean up new event horizon code

--- src/app/shared/components/event-horizon/event-horizon.component.ts
+++ src/app/shared/components/event-horizon/event-horizon.component.ts
@@ -1,13 +1,12 @@
-import { AfterViewInit, Component, ElementRef, HostBinding, NgZone, OnDestroy, OnInit } from "@angular/core";
+import { AfterViewInit, Component, ElementRef, NgZone, OnDestroy, OnInit } from "@angular/core";
 import { select, Store } from "@ngrx/store";
 import { gateClosed } from "app/dialing-computer/actions";
 import { getGateStatus } from "app/dialing-computer/selectors";
-import { EventHorizonAnimations } from "app/shared/animations";
 import { GateStatus } from "app/shared/models";
 import { AudioService } from "app/shared/services";
 import { gsap } from "gsap";
 import { clamp, random } from "lodash-es";
-import { Application, Assets, BlurFilter, Sprite } from "pixi.js";
+import { Application, Assets, BlurFilter, Resource, Sprite, Texture } from "pixi.js";
 import { Subject } from "rxjs";
 import { filter, takeUntil } from "rxjs/operators";
 
@@ -21,15 +20,10 @@ export class EventHorizonComponent implements AfterViewInit, OnDestroy, OnInit {
 	private eventHorizon: Application<HTMLCanvasElement>;
 	private readonly ignoredStatuses = [GateStatus.Dialing, GateStatus.Engaged];
 	private killSubscriptions: Subject<{}> = new Subject();
+	private texture: Texture<Resource>;
+	private readonly tints = [0xbbbbbb, 0xffffff, 0x95a5ab, 0x749efd, 0x99bdf9];
 
-	@HostBinding("class.active")
-	public active: boolean;
-
-	@HostBinding("class.anim-idle")
 	public animationIdle: boolean;
-
-	@HostBinding("class.anim-active")
-	public animationActive: boolean;
 
 	private get elem(): HTMLElement {
 		return this._elem.nativeElement;
@@ -80,92 +74,84 @@ export class EventHorizonComponent implements AfterViewInit, OnDestroy, OnInit {
 	}
 
 	private async animateEventHorizon(): Promise<void> {
-		const texture = await Assets.load("assets/images/event-horizon.svg");
+		this.texture ??= await Assets.load("assets/images/event-horizon.svg");
 
 		this.activeAnimationTimer = setInterval(() => {
-			animateParticle();
-			animateParticle();
-			animateParticle();
-			animateParticle();
-		}, 10);
-
-		const tints = [0xbbbbbb, 0xffffff, 0x95a5ab, 0x749efd, 0x99bdf9];
-
-		const animateParticle = () => {
-			// This creates a texture
-			const eventHorizonParticle = new Sprite(texture);
-			eventHorizonParticle.tint = tints[random(0, 4)];
-			eventHorizonParticle.scale.x = 0.2;
-			eventHorizonParticle.scale.y = 0.2;
-			eventHorizonParticle.x = this.eventHorizon.renderer.width / 2;
-			eventHorizonParticle.y = this.eventHorizon.renderer.height / 2;
-			eventHorizonParticle.anchor.x = 0.5;
-			eventHorizonParticle.anchor.y = 0.5;
-			eventHorizonParticle.alpha = 0.25;
-			const vector = random(0, 359);
-			let speedFactor = 0.25;
-			if (eventHorizonParticle.tint == 0xffffff) {
-				this.eventHorizon.stage.addChild(eventHorizonParticle);
-			} else {
-				this.eventHorizon.stage.addChildAt(eventHorizonParticle, 0);
+			for (let i = 0; i < 4; i++) {
+				this.animateParticle();
 			}
+		}, 10);
+	}
 
-			const originalX = eventHorizonParticle.position.x;
-			const originalY = eventHorizonParticle.position.y;
-			this.eventHorizon.ticker.add(() => {
-				const totalChangeX = Math.abs(originalX - eventHorizonParticle.position.x);
-				const totalChangeY = Math.abs(originalY - eventHorizonParticle.position.y);
+	private animateParticle(): void {
+		const eventHorizonParticle = new Sprite(this.texture);
+		eventHorizonParticle.alpha = 0.25;
+		eventHorizonParticle.anchor.x = 0.5;
+		eventHorizonParticle.anchor.y = 0.5;
+		eventHorizonParticle.scale.x = 0.2;
+		eventHorizonParticle.scale.y = 0.2;
+		eventHorizonParticle.tint = this.tints[random(0, 4)];
+		eventHorizonParticle.x = this.eventHorizon.renderer.width / 2;
+		eventHorizonParticle.y = this.eventHorizon.renderer.height / 2;
 
-				const xChange = Math.cos(vector) * clamp(speedFactor, 0.25, 0.5);
-				const yChange = Math.sin(vector) * clamp(speedFactor, 0.25, 0.5);
+		const vector = random(0, 359);
+		let speedFactor = 0.25;
 
-				speedFactor += 0.001;
+		if (eventHorizonParticle.tint == 0xffffff) {
+			this.eventHorizon.stage.addChild(eventHorizonParticle);
+		} else {
+			this.eventHorizon.stage.addChildAt(eventHorizonParticle, 0);
+		}
 
-				eventHorizonParticle.position.x += xChange;
-				eventHorizonParticle.position.y += yChange;
+		const originalX = eventHorizonParticle.position.x;
+		const originalY = eventHorizonParticle.position.y;
 
-				if (eventHorizonParticle.tint == 0xffffff && Math.max(totalChangeX, totalChangeY) > 100) {
-					eventHorizonParticle.alpha -= 0.0025;
-				}
-				if (Math.max(totalChangeX, totalChangeY) > 150) {
-					eventHorizonParticle.alpha -= 0.0025;
-				}
-				if (eventHorizonParticle.alpha < 0) {
-					this.eventHorizon.stage.removeChild(eventHorizonParticle);
-				}
-			});
-		};
+		this.eventHorizon.ticker.add(() => {
+			const totalChangeX = Math.abs(originalX - eventHorizonParticle.position.x);
+			const totalChangeY = Math.abs(originalY - eventHorizonParticle.position.y);
+			const clampedSpeedFactor = clamp(speedFactor, 0.25, 0.5);
+
+			const xChange = Math.cos(vector) * clampedSpeedFactor;
+			const yChange = Math.sin(vector) * clampedSpeedFactor;
+
+			speedFactor += 0.001;
+
+			eventHorizonParticle.position.x += xChange;
+			eventHorizonParticle.position.y += yChange;
+
+			if (eventHorizonParticle.tint == 0xffffff && Math.max(totalChangeX, totalChangeY) > 100) {
+				eventHorizonParticle.alpha -= 0.0025;
+			}
+			if (Math.max(totalChangeX, totalChangeY) > 150) {
+				eventHorizonParticle.alpha -= 0.0025;
+			}
+			if (eventHorizonParticle.alpha < 0) {
+				this.eventHorizon.stage.removeChild(eventHorizonParticle);
+			}
+		});
 	}
 
 	private createEventHorizon(): void {
-		// The application will create a canvas element for you that you
-		// can then insert into the DOM
 		this.eventHorizon = new Application<HTMLCanvasElement>({ resizeTo: this.elem, backgroundAlpha: 0 });
-		this.eventHorizon.renderer.background;
+		this.eventHorizon.stage.filters = [new BlurFilter(1)];
 		this.eventHorizon.view.style.position = "absolute";
-		this.eventHorizon.view.style.top = "0";
-		this.eventHorizon.view.style.left = "0";
 		this.eventHorizon.view.style.bottom = "0";
+		this.eventHorizon.view.style.left = "0";
 		this.eventHorizon.view.style.right = "0";
+		this.eventHorizon.view.style.top = "0";
 		this.elem.appendChild(this.eventHorizon.view);
-
-		const blurFilter = new BlurFilter(1);
-		this.eventHorizon.stage.filters = [blurFilter];
-
-		// load the texture we need
 	}
 
 	private stopEventHorizon(): void {
 		clearInterval(this.activeAnimationTimer);
-		let clearFn;
-		this.eventHorizon.ticker.add(
-			(clearFn = () => {
-				this.eventHorizon.stage.children.forEach((particle) => (particle.alpha -= 0.0005));
+		const clearFn = () => {
+			this.eventHorizon.stage.children.forEach((particle) => (particle.alpha -= 0.0005));
 
-				if (this.eventHorizon.stage.children.length === 0) {
-					this.eventHorizon.ticker.remove(clearFn);
-				}
-			}),
-		);
+			if (this.eventHorizon.stage.children.length === 0) {
+				this.eventHorizon.ticker.remove(clearFn);
+			}
+		};
+
+		this.eventHorizon.ticker.add(clearFn);
 	}
 }
